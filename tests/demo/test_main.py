756deab83038ca38123f0ef48b7a306073bbb211
pdemeulenaer <pdemeulenaer@gmail.com>
2020-12-14 17:26:30 +0200

    adapt to pipenv

---
+++ tests/demo/test_main.py
@@ -0,0 +1,17 @@
+"""Test main module."""
+
+from demo.main import main
+
+
+def test_main():
+    """Test whether main function works."""
+    expected_value = '👍🤞👌🐌'
+    assert all([
+        row.added_column == expected_value
+        for row
+        in (
+            main('mnt/demo/*.csv', expected_value)
+            .select('added_column')
+            .collect()
+        )
+    ])
