769504568589b3c3699e8a46527eeb0ee7eafc26
pdemeulenaer <pdemeulenaer@gmail.com>
2020-12-16 18:39:28 +0200

    correct path of csf file in main

--- src/demo/main.py
+++ src/demo/main.py
@@ -1,17 +1,26 @@
+import os
 import sys
 
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as sf
+from pyspark.sql import functions as psf
 
 def main(filepath, value):
     """Read in data, add a column `added_column`."""
     spark = SparkSession.builder.getOrCreate()
 
+    spark_home = os.environ.get('SPARK_HOME', None)
+    if 'databricks' in spark_home:
+        cwd="/"        
+    else:
+        cwd=""
+
+    print(spark_home)
+
     return (
         spark.read
         .option('inferSchema', True)
-        .csv(filepath, header=True)
-        .withColumn('added_column', sf.lit(value))
+        .csv(cwd+filepath, header=True)
+        .withColumn('added_column', psf.lit(value))
     )
 
 if __name__ == "__main__":
