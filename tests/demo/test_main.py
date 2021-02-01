d1c62d50947371d8e1fff24c7d69ed018f928ce8
Philippe de Meulenaer <31767740+pdemeulenaer@users.noreply.github.com>
2021-02-01 13:20:24 +0200

    add volonteer mistake to make test fail

--- tests/demo/test_main.py
+++ tests/demo/test_main.py
@@ -5,7 +5,7 @@ from demo.main import main
 
 def test_main():
     """Test whether main function works."""
-    expected_value = '👍🤞👌🐌'
+    expected_value = '👍🤞👌🐌0000000000'
     assert all([
         row.added_column == expected_value
         for row
